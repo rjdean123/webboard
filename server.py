4887642a85672925eb9ff0cb24af057511d2a4c8
ryan dean <rjd9se@virginia.edu>
2017-04-07 23:30:14 -0400

    testcount

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+count = 0
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "visitor # " + str(count)
+    count+=1
 
 if __name__ == "__main__":
     app.run()
