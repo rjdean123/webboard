8314b2ac532cbe15bdfc95bb41c47e16b1a5e2df
ryan dean <rjd9se@virginia.edu>
2017-04-08 00:06:58 -0400

    switching back to flask

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 def hello():
     return "Hello!"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+	app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
