abd496c3842745db2b2847b5b734ce231b9afbf4
ryan dean <rjd9se@virginia.edu>
2017-04-08 00:00:26 -0400

    switching back to flask

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
-#900x600
-
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def main_get():
-    return "hello world"
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run()
